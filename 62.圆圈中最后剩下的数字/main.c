7853d889f75ad975285db246fb243dfeddd5c7fa
Hui Su <sh_def@163.com>
2020-11-30 01:33:34 +0800

    Add topic to 68
    Add topic to 68
    
    Signed-off-by: Hui Su <sh_def@163.com>

---
+++ 62.圆圈中最后剩下的数字/main.c
@@ -0,0 +1,21 @@
+#include "common_c.h"
+#include "offer_common.h"
+
+int lastRemaining(int n, int m){
+
+}
+
+/**
+ * @brief test main()
+ *
+ * @param argc
+ * @param argv
+ * @return int
+ */
+int main(int argc, char **argv)
+{
+    leetcode_func();
+    printf("result is %d\n", 1);
+
+    return 1;
+}
